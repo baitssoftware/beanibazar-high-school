f183f65c149613461c76dceaf4e883de848aa4d9
sabbirsami <smd71430@gmail.com>
2025-03-10 11:34:36 +0600

    add git

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -9,7 +9,7 @@ const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
     <>
       <Head>
         <title>{schoolName}</title>
-        <meta property="og:title" content="Beanibazar Ideal Madrasah" key="title" />
+        <meta property="og:title" content="Beanibazar Ideal Madrasah kocu" key="title" />
       </Head>
       <Navbar />
       <main className="max-w-7xl mx-auto min-h-[50vh] bg-white px-4 shadow-md">{children}</main>
