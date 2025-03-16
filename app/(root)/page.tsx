15bc9fca17c6679c0e8082b367f78c2c851934f4
sabbirsami <smd71430@gmail.com>
2025-03-16 09:36:36 +0600

    fix loading color

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -95,7 +95,7 @@ const Home = () => {
         <div role="status">
           <svg
             aria-hidden="true"
-            className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+            className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-primary_school"
             viewBox="0 0 100 101"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
