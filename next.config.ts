03ba27ff89b064a04cd4e9b9c548495ab22f5038
Bangladesh Associate of IT Solution <74583888+baitssoftware@users.noreply.github.com>
2025-06-13 00:52:31 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from 'next';
 
+
+
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    domains: ['dstqm.nedubd.com'], 
+
     remotePatterns: [
       {
         protocol: 'https',
@@ -12,55 +16,6 @@ const nextConfig: NextConfig = {
   },
 };
 
-// const nextConfig: NextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'i.pinimg.com',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'placehold.co',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'images.pexels.com',
-//       },
-//       {
-//         protocol: 'http',
-//         hostname: 'localhost',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'task.bamsbd.com',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'shinystepsedu.com',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'api.shinystepsedu.com',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'test.bamsbd.com',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'dstqm.nedubd.com',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'backend-beanibazar-ideal-madrasah.vercel.app',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'bb',
-//       },
-//     ],
-//   },
-// };
+
 
 export default nextConfig;
